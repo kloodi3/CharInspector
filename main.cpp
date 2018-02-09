f2e79f7ff24ed99335c03029c295f398a28a0071
kloodi3 <kmalrashdi@mail.csuchico.edu>
2018-02-08 20:18:17 -0800

    Character Inspector

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ if(letter>='A' && letter<='Z') {
 
 
   //when user's entry is between A-Z...
-  cout<<" is an upper case letter!\n";
+  cout<<letter<<" is an upper case letter!\n";
 } 
 
 
@@ -21,12 +21,12 @@ else if (letter>='a' && letter<='z') {
 
 
   //when user's entry is between a-z...
-  cout<<" is a lower case letter!\n";
+  cout<<letter<<" is a lower case letter!\n";
 }
 
 else { 
   //in all other cases...
-  cout<<"?! Pssh. What are you talking about?\n";
+  cout<<letter<<"?! Pssh. What are you talking about?\n";
   
   //no matter what they enter...
   cout<<"The ASCII value is: "<<(int)letter<<endl;
